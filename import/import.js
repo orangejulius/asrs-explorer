48d8a246c7aa25a2bcd443e0b1f22f9cf366b7b4
Julian Simioni <julian@simioni.org>
2015-11-08 17:04:07 -0500

    Only include date fixing stream for now

--- import/import.js
+++ import/import.js
@@ -2,6 +2,8 @@ var csv_parse = require('csv-parse');
 var fs = require('fs');
 
 var objectExtractorStream = require('./src/objectExtractorStream');
+var dateFixStream = require('./src/dateFixStream');
+var numberParsingStream = require('./src/numberParsingStream');
 var esUpdateStream = require('./src/esUpdateStream');
 
 var parser = csv_parse({delimiter: ','});
@@ -11,4 +13,5 @@ var filename = process.argv[2];
 fs.createReadStream(filename)
 .pipe(parser)
 .pipe(objectExtractorStream)
+.pipe(dateFixStream)
 .pipe(esUpdateStream)
