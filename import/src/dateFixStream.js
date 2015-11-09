adcfbd85506848dd79bd9879c21e73ca4a774b99
Julian Simioni <julian@simioni.org>
2015-11-09 09:57:25 -0500

    Handle missing months in dates

--- import/src/dateFixStream.js
+++ import/src/dateFixStream.js
@@ -5,6 +5,10 @@ var pipe = through.obj(function(chunk, encoding, callback) {
         var year = chunk.value.substring(0, 4);
         var month = chunk.value.substring(4);
         var day = '01'; // Records do not store day so invent one
+
+        if (month.length === 0) {
+            month = '01'; // month can be missing as well
+        }
         var date = year + '-' + month + '-' + day;
         chunk.value = date;
     }
